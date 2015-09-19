c33193dadd3ca70e4b5928046b0ac50d0af059df
idevz <15134559390@163.com>
2015-09-19 17:53:31 +0800

    bluebird
    move on

--- application/controllers/index.lua
+++ application/controllers/index.lua
@@ -1,6 +1,16 @@
 local IndexController = {}
+local service = require 'models.service.user'
 
 function IndexController:index()
+	-- local sgmatch = ngx.re.gmatch or string.gmatch
+	-- ppz(package.config)
+	-- pp(package.loaders)
+	-- pp(package.searchers)
+	-- for k,v in pairs(package) do
+	-- 	pp(k)
+	-- end
+	-- pp(package.preload)
+	-- pp(package.loaders)
     local view = self:getView()
     local p = {}
     p['vanilla'] = 'Welcome To Vanilla...'
--- config/application.lua
+++ config/application.lua
@@ -1,10 +1,10 @@
 local Appconf={}
-Appconf.name = 'a'
+Appconf.name = 'bluebird'
 
 Appconf.route='vanilla.v.routes.simple'
 Appconf.bootstrap='application.bootstrap'
 Appconf.app={}
-Appconf.app.root='../' .. Appconf.name .. '/'
+Appconf.app.root='./'
 
 Appconf.controller={}
 Appconf.controller.path=Appconf.app.root .. 'application/controllers/'
