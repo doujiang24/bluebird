415ec7f5a75c51cc76b667158c182ade20245446
idevz <15134559390@163.com>
2015-10-16 15:03:34 +0800

    docker pull zhoujing/vanilla

--- spec/application/controllers/index_spec.lua
+++ spec/application/controllers/index_spec.lua
@@ -12,7 +12,7 @@ describe("IndexController", function()
             assert.are.same([[<!DOCTYPE html>
 <html>
 <body>
-  <img src="http://m1.sinaimg.cn/maxwidth.300/m1.sinaimg.cn/ca65fa784406a36ba4fc41d14e21661e_1364_1494.png">
+  <img src="http://m1.sinaimg.cn/maxwidth.300/m1.sinaimg.cn/120d7329960e19cf073f264751e8d959_2043_2241.png">
   <h1><a href = 'https://github.com/idevz/vanilla'>Welcome To Vanilla...</a></h1><h5>Power by Openresty</h5>
 </body>
 </html>
